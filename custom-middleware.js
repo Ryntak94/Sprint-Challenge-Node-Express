b9ca07362876dcf776a8660e293e0f7efbccafee
Ryntak94 <ryan.matthews.professional@gmail.com>
2018-12-08 12:49:04 -0500

    Fix bug in custom middleware

--- custom-middleware.js
+++ custom-middleware.js
@@ -1,8 +1,8 @@
 const maxNameLength = (req, res, next)  =>  {
-    const user = req.body.user;
-    if(user === undefined)  {
+    const name = req.body.name;
+    if(name === undefined)  {
         next();
-    }   else if(user.length > 128)   {
+    }   else if(name.length > 128)   {
         res.status(422).json({ message: "Name is too long. Please submit a shorter name."});
     }   else {
         next();
@@ -13,7 +13,7 @@ const completedIsBoolean = (req, res, next) =>  {
     const { completed } = req.body;
     if(completed === undefined)    {
         next();
-    }   else if(typeof completed !== boolean)   {
+    }   else if(typeof completed !== "boolean")   {
         res.status(422).json({ message: "Please make sure 'completed' is a boolean (true or false)." });
     }   else {
         next();
@@ -22,9 +22,10 @@ const completedIsBoolean = (req, res, next) =>  {
 
 const descriptionIsString = (req, res, next)    =>  {
     const { description }   =   req.body;
+    console.log()
     if(description === undefined)   {
         next();
-    }   else if(typeof completed !== string)   {
+    }   else if(typeof description !== "string")   {
         res.status(422).json( { message: "Please make sure the description is a String." });
     }   else {
         next();
